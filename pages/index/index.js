3b09dcabf18f6206f35673649aeaf10d74b63bdc
zzd <949759694@qq.com>
2019-03-18 09:40:28 +0800

    首页组件组合‘

--- pages/index/index.js
+++ pages/index/index.js
@@ -29,12 +29,12 @@ function initChart(canvas, width, height) {
           lineStyle: {
             width: 18,
             shadowBlur: 0,
-            color: [[0.3, "#67e0e3"], [0.7, "#37a2da"], [1, "#fd666d"]]
+            color: [[0.4, "#fd666d"], [0.8, "#37a2da"], [1, "#67e0e3"]]
           }
         },
         data: [
           {
-            value: 40
+            value: 95
           }
         ]
       }
